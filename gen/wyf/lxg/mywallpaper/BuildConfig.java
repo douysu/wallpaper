fd3eb612a4891f2bf1d2003080346915ba91919e
douysu <yindou97@163.com>
2020-06-02 22:59:43 +0800

    [del] 整合

--- gen/wyf/lxg/mywallpaper/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package wyf.lxg.mywallpaper;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/wyf/lxg/mywallpaper/LiveWallpaper.java
+++
@@ -1,9 +0,0 @@
-package wyf.lxg.mywallpaper;
-
-public class LiveWallpaper extends OpenGLES3WallpaperService{
-	@Override
-	android.opengl.GLSurfaceView.Renderer getNewRenderer() {
-		// TODO Auto-generated method stub
-		return new MySurfaceView(this);//创建自定义场景渲染器
-	}
-}
